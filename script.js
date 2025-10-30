5903f073cc266e3137ce9876b12c9b2855f27243
TChamba Romial <chambaromial@gmail.com>
2025-10-29 18:05:27 -0700

    Second commit with updated api keys from moviedatabase

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const APILINK = '';
-const IMG_PATH = '';
-const SEARCHAPI = '';
+API_LINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=42534b9b97fd0ef8fef94e03dc8b6542&page=1'
+IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=42534b9b97fd0ef8fef94e03dc8b6542&query="'
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
+returnMovies(API_LINK);
+
 function returnMovies(url) {
-    fetch(url).then(res => res.json())
-    .then(function(data)) {
+    fetch(url)
+    .then(res => res.json())
+    .then(function(data) {
         console.log(data.result);
         data.results.forEach(element => {
             const div_card = document.createElement('div');
@@ -22,14 +25,14 @@ function returnMovies(url) {
 
             const image = document.createElement('img');
             image.setAttribute('class', 'thumbnail');
-            image.setAttribute('class', 'image');
+            image.setAttribute('id', 'image');
 
             const title = document.createElement('h3');
             title.setAttribute('class', 'title');
 
             const center = document.createElement('center');
 
-            title.innerHTML = `${element.title}`;
+            title.innerHTML = `${element.title.slice(0, 21)}`;
             image.src = IMG_PATH + element.poster_path;
 
             center.appendChild(image);
@@ -39,17 +42,18 @@ function returnMovies(url) {
             div_row.appendChild(div_column);
 
             main.appendChild(div_row);
-        })
-    }
+        });
+    });
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
     main.innerHTML = '';
     const searchItem = search.value;
 
     if(searchItem) {
-        returnMovies(SEARCHAPI + searchItem);
+        returnMovies(SEARCH_API + searchItem);
         search.value = "";
     }
 });
